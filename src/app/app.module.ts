fb579e55030b75499f2898d649713227121b0bbb
Tshepho <tshepomoshabane3@gmail.com>
2021-08-12 09:56:44 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatGridListModule,  
+
+    AppRoutingModule,
+    MatCardModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatButtonModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,72 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router:Router) { }
+
+  TotalAmount = 0;
+  state = "disabled";
+  temp = [{ AccountNum: 0, AccountType: "", amount: 0, State: "" }]
+
+  constructor(private router: Router) {
+
+  }
+
+
+  accountList = [
+    { AccountNum: 6331103626640816, AccountType: 'Cheque', amount: -296.65 },
+    { AccountNum: 5248117462997084, AccountType: 'Savings', amount: -20.00 },
+    { AccountNum: 3581474249964105, AccountType: 'Savings', amount: 980.20 },
+    { AccountNum: 6709502417011722, AccountType: 'Savings', amount: 905.81 },
+    { AccountNum: 5308160489139568, AccountType: 'Cheque', amount: -986.10 },
+    { AccountNum: 3559243852997209, AccountType: 'Cheque', amount: 531.75 },
+    { AccountNum: 3585913435866604, AccountType: 'Cheque', amount: 253.14 },
+    { AccountNum: 3564003756077737, AccountType: 'Savings', amount: 896.79 },
+    { AccountNum: 3543910523783643, AccountType: 'Cheque', amount: -590.47 },
+    { AccountNum: 3532070362684767, AccountType: 'Savings', amount: 58.00 },
+  ];
 
   ngOnInit(): void {
-    this.router.navigate(['Home'])
+    this.getTotalAmount()
+  }
+
+  getTotalAmount() {
+    this.temp.splice(1, 1);
+    this.accountList.forEach(a => {
+      this.TotalAmount = this.TotalAmount + a.amount;
+      
+
+      switch (a.AccountType) {
+        case 'Cheque':
+        
+          if (a.amount > -501) {
+            this.state="Active"
+          }else{
+            this.state="Disabled"
+          }
+
+          break;
+
+          case 'Savings':
+          
+          if (a.amount>0) {
+            this.state="Active"
+          }else{
+            this.state="Disabled"
+          }
+
+          break;
+      
+        default:
+          break;
+      }
+
+      this.temp.push({ AccountNum: a.AccountNum, AccountType: a.AccountType, amount: a.amount, State: this.state })
+
+    });
+  }
+
+
+  notify() {
+    alert("Success");
   }
 
 }
